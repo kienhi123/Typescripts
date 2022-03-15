1bdad88edc109e8215772f1652afb6442e42f24b
kienhi123 <kien123@gmail.com>
2022-03-15 23:00:56 +0700

    first commit

---
+++ dist/app.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const a = 10;
+const b = 20;
+const myName = "Le Trong Dat";
+const myAge = 20;
+const myStatus = true;
+const myObj = { id: 1, name: "Dat", price: 20 };
+const arrNumber = [1, 2, 3, 4];
+const arrString = ["a", "b", "cd"];
+const arrObj = [
+    { id: 1, name: "San pham A", price: 500 },
+    { id: 1, name: "San pham ABC", status: true }
+];
